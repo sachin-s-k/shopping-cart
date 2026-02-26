5b04199de24ef71f010d628c835ee38d0f424f5f
officialsachin <sachin@litschool.in>
2026-02-26 11:25:54 +0530

    shopping cart project initialized

---
+++ src/test/java/com/sachin_s_k/shopping_cart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sachin_s_k.shopping_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
